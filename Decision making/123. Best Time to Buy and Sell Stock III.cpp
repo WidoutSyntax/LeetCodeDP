936fc681dfca8a3ad71f38bf6fdc258bf62a88e9
Pratik <pratikpawar16aur@gmail.com>
2021-07-20 17:17:27 +0530

    Change in best time to buy and sell stocks III

--- Decision making/123. Best Time to Buy and Sell Stock III.cpp
+++ Decision making/123. Best Time to Buy and Sell Stock III.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> DP(3, vector<int>(n, -1));
-        for (int j = 0; j < n; j++) DP[0][j] = 0;
-        for (int i = 0; i <= 2; i++) DP[i][0] = 0;
+        int fb = INT_MIN, sb = INT_MIN;
+        int fs = 0, ss = 0;
 
-        for (int i = 1; i <= 2; i++)
+        for (int i = 0; i < n; i++)
         {
-            int mx = DP[i - 1][0] - prices[0];
-            for (int j = 1; j < n; j++)
-            {
-                DP[i][j] = max(DP[i][j - 1], prices[j] + mx);
-                mx = max(mx, DP[i - 1][j] - prices[j]);
-            }
+            fb = max(fb, -prices[i]);
+            fs = max(fs, fb + prices[i]);
+            sb = max(sb, fs - prices[i]);
+            ss = max(ss, sb + prices[i]);
         }
 
-        return DP[2][n - 1];
+        return ss;
     }
 };
